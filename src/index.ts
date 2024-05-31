6477ddb0d09e6b90f46a2ded5fd294998d3d0db0
Arne Diekmann <arne@neoskop.de>
2024-05-31 10:47:28 +0200

    chore: Bump version to 2.1.4.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { S3Error } from './s3.error.js';
 import { S3Surgeon } from './s3surgeon.js';
 
 program
-  .version('2.1.3', '-v, --version')
+  .version('2.1.4', '-v, --version')
   .option('-k, --access-key-id <access-key-id>', 'AWS Access Key ID')
   .option(
     '-s, --secret-access-key <secret-access-key>',
